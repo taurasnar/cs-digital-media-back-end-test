f81b524756f4492f42acb38d7439b5ee2d2aec9a
Tauras Narbutas <T.Narbutas@student.tudelft.nl>
2022-01-13 21:01:07 +0100

    Upload project.

---
+++ src/main/java/csdigitalmediabackendtest/CsDigitalMediaBackEndTestApplication.java
@@ -0,0 +1,13 @@
+package csdigitalmediabackendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsDigitalMediaBackEndTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsDigitalMediaBackEndTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csdigitalmediabackendtest/CsDigitalMediaBackEndTestApplicationTests.java
@@ -0,0 +1,13 @@
+package csdigitalmediabackendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsDigitalMediaBackEndTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
